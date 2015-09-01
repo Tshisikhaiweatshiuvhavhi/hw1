ebb66e845be7852a4902539223abb1d44d325337
Tshisikhaiweatshiuvhavhi <832212@students.wits.ac.za>
2015-09-01 16:53:04 +0200

    PART4

--- hw1/shell.c
+++ hw1/shell.c
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdbool.h>
+#include <fcntl.h> 
 
 #define INPUT_STRING_SIZE 80
 
@@ -25,6 +26,14 @@ int cmd_quit(tok_t arg[]) {
 int cmd_help(tok_t arg[]);
 
 
+int cmd_cd(tok_t arg[]){
+  if(arg[0] == NULL) return 0;
+  else
+    chdir(arg[0]);
+}
+
+
+
 /* Command Lookup table */
 typedef int cmd_fun_t (tok_t args[]); /* cmd functions take token array and return int */
 typedef struct fun_desc {
@@ -36,6 +45,7 @@ typedef struct fun_desc {
 fun_desc_t cmd_table[] = {
   {cmd_help, "?", "show this help menu"},
   {cmd_quit, "quit", "quit the command shell"},
+  {cmd_cd,  "cd","change directory"},
 };
 
 int cmd_help(tok_t arg[]) {
@@ -58,29 +68,30 @@ void init_shell()
 {
   /* Check if we are running interactively */
   shell_terminal = STDIN_FILENO;
-
+  
   /** Note that we cannot take control of the terminal if the shell
-      is not interactive */
+   is not interactive */
   shell_is_interactive = isatty(shell_terminal);
-
+  
   if(shell_is_interactive){
-
+    
     /* force into foreground */
     while(tcgetpgrp (shell_terminal) != (shell_pgid = getpgrp()))
       kill( - shell_pgid, SIGTTIN);
-
+    
     shell_pgid = getpid();
     /* Put shell in its own process group */
     if(setpgid(shell_pgid, shell_pgid) < 0){
       perror("Couldn't put the shell in its own process group");
       exit(1);
     }
-
+    
     /* Take control of the terminal */
     tcsetpgrp(shell_terminal, shell_pgid);
     tcgetattr(shell_terminal, &shell_tmodes);
   }
   /** YOUR CODE HERE */
+  
 }
 
 /**
@@ -96,12 +107,69 @@ void add_process(process* p)
  */
 process* create_process(char* inputString)
 {
+  pid_t tcpid;
+  pid_t pid = getpid(); // get pid of the current process ...
+  // printf( "Parent pid: %d\n", pid ); // ... and print it out
+  pid_t cpid;
+  pid_t mypid;
+  
+  
   /** YOUR CODE HERE */
   return NULL;
 }
 
-
-
+char* concat(char *s1, char *s2)
+{
+  char *result = malloc(strlen(s1)+strlen(s2)+1);//+1 for the zero-terminator
+  //in real code you would check for errors in malloc here
+  strcpy(result, s1);
+  strcat(result, s2);
+  return result;
+}
+void part2(tok_t * t){
+    execv(*t,t);
+      perror(*t);
+      exit(0);
+}
+void part3(tok_t *t){
+  char *poi=getenv("PATH");
+      tok_t * pois = getToks(poi);
+      int i;
+      for(i = 0;i<MAXTOKS && pois[i];i++){
+        //char *( char *pois, *t);
+        char *fi=concat(pois[i],"/");
+        fi=concat(fi,t[0]);
+        if(access(fi,F_OK) != -1){
+          execve(fi,t, NULL);
+         printf("%s \n",fi);
+        }
+        //perror(*t);
+      }
+}
+void part4(tok_t *input,char * filename,char * c){
+int newfd;
+ 
+ if(c == ">"){
+  //printf("PART 4\n");
+  if ((newfd = open(filename, O_CREAT|O_WRONLY | O_APPEND, 0644)) < 0) {
+	perror(input);
+	exit(1);
+  }
+ 
+ dup2(newfd, 1);
+ close(newfd);
+ }
+ if(c == "<"){
+  if ((newfd = open(filename, O_RDONLY, 0644)) < 0) {
+	perror(input);
+	exit(1);
+  }
+ dup2(newfd,0);
+ close(newfd);
+ }
+  part3(input);
+  part2(input);
+}
 int shell (int argc, char *argv[]) {
   char *s = malloc(INPUT_STRING_SIZE+1);			/* user input string */
   tok_t *t;			/* tokens parsed from input */
@@ -110,21 +178,48 @@ int shell (int argc, char *argv[]) {
   pid_t pid = getpid();		/* get current processes PID */
   pid_t ppid = getppid();	/* get parents PID */
   pid_t cpid, tcpid, cpgid;
-
+  
   init_shell();
-
+  
   printf("%s running as PID %d under %d\n",argv[0],pid,ppid);
-
+  
   lineNum=0;
-  fprintf(stdout, "%d: ", lineNum);
+  char cwd[5000];
+  fprintf(stdout, "%d: %s:", lineNum, getcwd(cwd,sizeof(cwd)));
   while ((s = freadln(stdin))){
     t = getToks(s); /* break the line into tokens */
-    fundex = lookup(t[0]); /* Is first token a shell literal */
-    if(fundex >= 0) cmd_table[fundex].fun(&t[1]);
-    else {
-      fprintf(stdout, "This shell only supports built-ins. Replace this to run programs as commands.\n");
+  fundex = lookup(t[0]); /* Is first token a shell literal */
+  if(fundex >= 0) cmd_table[fundex].fun(&t[1]);
+  else {
+    pid = fork(); 
+    
+    if( pid == 0 ){ // child process
+     // printf("%d\n", strlen(strstr(s,">")));
+         int i;
+         for(i=0;i<MAXTOKS && t[i];i++){
+		 if (strcmp( t[i], ">") == 0){
+	 	  t[i]=NULL;
+		  //printf("hello");
+		  part4(t,t[i+1],">");	
+                  }
+		if (strcmp( t[i], "<") == 0){
+	 	  t[i]=NULL;
+		  //printf("hello");
+		  part4(t,t[i+1],"<");	
+                  }
+		}
+      part3(t);
+      part2(t);
+
+    }else if(pid<0){
+      perror( "Fork failed" );
+      exit( EXIT_FAILURE );    
     }
-    fprintf(stdout, "%d: ", lineNum);
+  }
+  lineNum++;
+  wait(NULL);
+  
+  fprintf(stdout, "%d: %s :", lineNum, getcwd(cwd,sizeof(cwd)));
   }
   return 0;
 }
